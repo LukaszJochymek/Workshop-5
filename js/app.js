7627b00c60d226ff160dc41f9356bc0ddbc21cec
LukaszJochymek <Lukasz.jochymek@interia.com>
2023-03-17 10:52:52 +0100

    create index and script

---
+++ js/app.js
@@ -0,0 +1,1 @@
+const key = "1d89d947-2445-40d1-ba74-34c44eabfe3f";
---
+++ script.js
@@ -0,0 +1,8 @@
+const apikey = '1d89d947-2445-40d1-ba74-34c44eabfe3f';
+const apihost = 'https://todo-api.coderslab.pl';
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
